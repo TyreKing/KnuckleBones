27e034fd3c31857a740752e2a0afdec0582c218c
tyreking <tyr3.k1ng@gmail.com>
2025-01-04 21:06:10 -0500

    use vanilla js

--- knuckle-bones/src/index.js
+++ knuckle-bones/src/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function rollDie(player) {
+
+    let turn = isInt(player);
+    if (turn) {
+        const min = 1;
+        const max = 6;
+        
+        const die = Math.floor(Math.random() * (max - min + 1)) + min;
+        if ( player === 1) {
+            let element = document.getElementById("p1-board").getElementsByTagName('h2');
+            element[0].innerHTML = die;
+        }
+        else {
+            let element = document.getElementById("p2-board").getElementsByTagName('h2');
+            element[0].innerHTML = die;
+        }
+    }
+}
+
+function isInt(value) {
+    if (isNaN(value)) {
+      return false;
+    }
+    var x = parseFloat(value);
+    return (x | 0) === x;
+}
+
