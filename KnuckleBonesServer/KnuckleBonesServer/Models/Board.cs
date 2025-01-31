d130cf287d6257adfbd2e51c454d31227f39ecba
tyre king <tyr3.k1ng@gmail.com>
2025-01-30 23:04:21 -0500

    Add basic server service methods

---
+++ KnuckleBonesServer/KnuckleBonesServer/Models/Board.cs
@@ -0,0 +1,63 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace KnuckleBonesServer.Models
+{
+    public class Board
+    {
+        public Dictionary<int, Column> Columns { get; set; } = [];
+
+        public Board() 
+        { 
+            Columns = new Dictionary<int, Column>();
+            for(int i = 0; i < 3; i++)
+            {
+                Columns.Add(i, new Column());
+            }
+        }
+    }
+
+    public class Column
+    {
+        public int Total = 0;
+        public int[] Col {  get; set; } = [ 0, 0, 0 ];
+
+        public void SetTotal()
+        {
+            Total = Col.Sum();
+        }
+    }
+
+    public class Player
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public int Die { get; set; }
+    }
+
+    public class Game
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public Board Player1Board { get; set; }
+        public Board Player2Board { get; set; }
+        public Player Host { get; set; }
+        public Player Challenger { get; set; }
+
+        public bool IsHostTurn { get; set; } = true;
+
+        public Game(string code, Player host)
+        {
+            Id = Guid.NewGuid();
+            Code = code;
+            Player1Board = new Board();
+            Player2Board = new Board();
+            Host = host;
+        }
+    }
+
+    public class GameRequest()
+    {
+        public Player Player { get; set; }
+        public string Code { get; set;}
+    }
+}
---
+++ KnuckleBonesServer/KnuckleBonesServer/Services/GameService.cs
@@ -0,0 +1,70 @@
+﻿using KnuckleBonesServer.Models;
+using Microsoft.Extensions.Hosting;
+
+namespace KnuckleBonesServer.Services
+{
+    public class GameService
+    {
+        private Dictionary<string, Game> _games;
+
+        public GameService()
+        {
+            if (_games == null || !_games.Any())
+            {
+                _games = new Dictionary<string, Game>();
+            }
+        }
+
+        public Game CreateGame(Player player, string code)
+        {
+            var game = _games.FirstOrDefault(x => x.Key == code).Value;
+
+            //Create game if it does not exist
+            if (game == null)
+            {
+                game = new Game(code, player);
+                _games.Add(code, game);
+            }
+
+            return game;
+            
+        }
+
+        public Game? JoinGame(Player player, string code)
+        {
+            var game = _games.FirstOrDefault(x => x.Key == code).Value;
+            if (game == null)
+            {
+               return null;
+            }
+            game.Challenger = player;
+            return game;
+        }
+
+        public Player? GetDie(Player player, string code)
+        {
+            var game = _games.FirstOrDefault(x => x.Key == code).Value;
+            if (game == null)
+            {
+                return null;
+            }
+
+            var rand = new Random();
+            var die = rand.Next(1, 7);
+            if (game.Host.Id == player.Id && game.IsHostTurn)
+            {
+                game.Host.Die = die;
+                return game.Host;
+            }
+
+            if (game.Challenger.Id == player.Id && !game.IsHostTurn)
+            {
+                game.Challenger.Die = die;
+                return game.Challenger;
+            }
+
+            return null;
+        }
+
+    }
+}
